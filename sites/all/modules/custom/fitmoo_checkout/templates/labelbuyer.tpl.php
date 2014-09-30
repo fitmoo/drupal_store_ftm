989d3150f0a986f460e575a1d6cc08c566812dbd
Colin Olkowski <colkowski@fitmoo.com>
2014-09-30 18:59:41 -0400

    A shipping email is now sent to the buyer but it's incorrect in that it says it sent from the buyer, there's not address etc.

--- sites/all/modules/custom/fitmoo_checkout/templates/labelbuyer.tpl.php
+++ sites/all/modules/custom/fitmoo_checkout/templates/labelbuyer.tpl.php
@@ -1,8 +1,3 @@
-<!-- <p style="margin:0; font-weight:normal;">Good news! Someone purchased <?php //print $product;?>. For details please visit <a href="https://beta.fitmoo.com/dashboard/sales">Your Sales</a> at Fitmoo.com. We will cancel this transaction if the item is not shipped within <strong>5days</strong></p>
-<h1>Order Details</h1>
-
-Order #: <?php //print $orderID;?> 
-Placed on: <?php //print gmdate("Y-m-d\TH:i:s\Z", $orderDate);?>  -->
 <?php
 global $base_url;
 $path = $base_url.'/'.drupal_get_path('module', 'fitmoo_checkout');
